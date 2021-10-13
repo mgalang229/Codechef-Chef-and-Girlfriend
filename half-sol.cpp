805f66e6fe10e2a73d465315e81b90c6d057c266
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-10-13 15:29:47 +0800

    Update half-sol.cpp

--- half-sol.cpp
+++ half-sol.cpp
@@ -16,7 +16,7 @@ int main() {
 		double g_mins = stoi(time1.substr(3, 2));
 		double c_hr = stoi(time2.substr(0, 2));
 		double c_mins = stoi(time2.substr(3, 2));
-		// for the first plan: find the total minutes between 'time1' and 'time2' plus the waiting time for the girlfriend
+		// for the first plan: find the total minutes between 'time1' and 'time2' plus the time that the two of them goes back home
 		double first = (60 - c_mins) + (60 * (g_hr - (c_hr + 1))) + g_mins + dist;
 		cout << fixed << setprecision(1) << first << " ";
 		// for the second plan: there are two (2) cases
@@ -29,8 +29,7 @@ int main() {
 		// greater than the total minutes between Chef and his girlfriend, then it means
 		// they will meet at some point on the road, and to solve this, we need to
 		// calculate the middle point of the trip
-		// note: don't forget to remove the 'dist' from 'first' to remove the waiting time
-		// for chef's girlfriend
+		// note: don't forget to remove the 'dist' from 'first' to remove the time that the two of them goes back home
 		first -= dist;
 		if (dist * 2 <= first) {
 			cout << first;
